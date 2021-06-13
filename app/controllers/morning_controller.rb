5ea76a1fb40628c4075bd5256e94eaf86f30ebb8
Midori-Gift <okometaberoyo5009@gmail.com>
2021-06-13 22:16:33 +0000

    コントリビュージョン

---
+++ app/controllers/morning_controller.rb
@@ -0,0 +1,2 @@
+class MorningController < ApplicationController
+end
---
+++ test/controllers/morning_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MorningControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
